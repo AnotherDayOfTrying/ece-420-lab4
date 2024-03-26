c37da960c910fda2fd0b3b7fc6b980003dee1faa
Tobrown3 <saifhusnain@gmail.com>
2024-03-26 10:28:36 -0600

    Added some mpi stuff, untested

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 #include "Lab4_IO.h"
 #include "timer.h"
 
@@ -23,7 +24,9 @@ int main (int argc, char* argv[]){
     double start, end;
     FILE *ip;
     /* INSTANTIATE MORE VARIABLES IF NECESSARY */
-
+    int rank;
+    int local_n;
+    int npes;
     
     // load data 
     if ((ip = fopen("data_input_meta","r")) == NULL) {
@@ -37,6 +40,7 @@ int main (int argc, char* argv[]){
     // initialize variables
     r = malloc(nodecount * sizeof(double));
     r_pre = malloc(nodecount * sizeof(double));
+    double *global_r = malloc(nodecount * sizeof(double));
     
     GET_TIME(start);
     iterationcount = 0;
@@ -44,12 +48,37 @@ int main (int argc, char* argv[]){
         r[i] = 1.0 / nodecount;
     /* INITIALIZE MORE VARIABLES IF NECESSARY */
 
+    //MPI initialization
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    local_n = nodecount/npes;
+    int *recvcounts = malloc(npes * sizeof(int));
+    int *disp = malloc(npes * sizeof(int));
+    int total = 0;
+
+    // Partitioning per process
+    for (i = 0; i < npes; i++) {
+        if (i < nodecount % npes) {
+            recvcounts[i] = local_n + 1;
+        }
+        else {
+            recvcounts[i] = local_n;
+        }
+        disp[i] = total;
+        total = total + recvcounts[i];
+    }
+
+    int p_start = disp[rank];
+    int p_end = p_start + recvcounts[rank];
+
     // core calculation
     do{
         ++iterationcount;
         /* IMPLEMENT ITERATIVE UPDATE */
         vec_cp(r, r_pre, nodecount);
-        for (i = 0; i < nodecount; ++i) {
+        for (i = p_start; i < p_end; i++) {
             struct node i_node = nodehead[i];
             double summation = 0;
 
@@ -58,14 +87,17 @@ int main (int argc, char* argv[]){
             }
             r[i] = ((1 - DAMPING_FACTOR)/nodecount) + DAMPING_FACTOR * summation;
         }
+
+        MPI_Allgatherv(r + p_start, p_end - p_start, MPI_DOUBLE, global_r, recvcounts, disp, MPI_DOUBLE, MPI_COMM_WORLD);
         
     }while(rel_error(r, r_pre, nodecount) >= EPSILON);
+    MPI_Finalize();
     GET_TIME(end);
 
     Lab4_saveoutput(r, nodecount, end - start);
 
     // post processing
     node_destroy(nodehead, nodecount);
-    free(r); free(r_pre);
+    free(r); free(r_pre); free(global_r);
     return 0;
 }
