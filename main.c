6adb69b0103fd2153052d8d7162f0df31f90ee62
AnotherDayOfTrying <javierjustin7@gmail.com>
2024-03-27 21:59:57 -0600

    feat: final implementation

--- main.c
+++ main.c
@@ -24,9 +24,8 @@ int main (int argc, char* argv[]){
     double start, end;
     FILE *ip;
     /* INSTANTIATE MORE VARIABLES IF NECESSARY */
-    int rank;
-    int local_n;
-    int npes;
+    int rank; // process id (in MPI)
+    int npes; // number of processes (in MPI)
     
     // load data 
     if ((ip = fopen("data_input_meta","r")) == NULL) {
@@ -38,42 +37,45 @@ int main (int argc, char* argv[]){
     if (node_init(&nodehead, 0, nodecount)) return 254;
     
     // initialize variables
-    r = malloc(nodecount * sizeof(double));
-    r_pre = malloc(nodecount * sizeof(double));
-    double *global_r = malloc(nodecount * sizeof(double));
+    r = malloc(nodecount * sizeof(double)); // current vector of page ranks
+    r_pre = malloc(nodecount * sizeof(double)); // previous vector of page ranks
+    double *global_r = malloc(nodecount * sizeof(double)); // vector of page ranks (used for receival)
+    double *summation = malloc(nodecount * sizeof(double)); // vector of page ranks (used for eq3 summation)
     
-    GET_TIME(start);
-    iterationcount = 0;
+    
+    iterationcount = 0; // number of iterations
+    double START_PROB = 1.0 / nodecount; // initial probability for each node
     for ( i = 0; i < nodecount; ++i)
-        r[i] = 1.0 / nodecount;
+        r[i] = START_PROB; // start with 1/N probability for each node
     /* INITIALIZE MORE VARIABLES IF NECESSARY */
 
-    //MPI initialization
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &npes);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // MPI initialization
+    MPI_Init(&argc, &argv); // Initialize the MPI execution. May be used to pass the command line arguments to all processes
+    MPI_Comm_size(MPI_COMM_WORLD, &npes); // var holds the number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // var holds current process id (different between procs)
 
-    local_n = nodecount/npes;
-    int *recvcounts = malloc(npes * sizeof(int));
-    int *disp = malloc(npes * sizeof(int));
+    int local_n = nodecount/npes; // initial (floored) number of nodes per process
+    int *part_sizes = malloc(npes * sizeof(int)); // array for holding the number of nodes per process
+    int *displacement = malloc(npes * sizeof(int)); // array for holding the index displacement of each process
+    
+    // Partitioning per process (with uneven distribution, in case nodecount is not divisible by npes)
     int total = 0;
-
-    // Partitioning per process
     for (i = 0; i < npes; i++) {
         if (i < nodecount % npes) {
-            recvcounts[i] = local_n + 1;
+            part_sizes[i] = local_n + 1; 
         }
         else {
-            recvcounts[i] = local_n;
+            part_sizes[i] = local_n;
         }
-        disp[i] = total;
-        total = total + recvcounts[i];
+        displacement[i] = total;
+        total = total + part_sizes[i];
     }
 
-    int p_start = disp[rank];
-    int p_end = p_start + recvcounts[rank];
-
-    printf("%d: Start: %d | End: %d \n", rank, p_start, p_end);
+    int p_start = displacement[rank]; // start index of this processor's partition
+    int p_end = p_start + part_sizes[rank]; // end index of this processor's partition
+    
+    const double DAMP_START_PROB = (1.0 - DAMPING_FACTOR)/nodecount; // const used in first half of eq 3
+    GET_TIME(start); // only measure the time required to run the PageRank algorithm and the necessary distributed computing overhead
 
     // core calculation
     do{
@@ -81,30 +83,33 @@ int main (int argc, char* argv[]){
         /* IMPLEMENT ITERATIVE UPDATE */
         vec_cp(r, r_pre, nodecount);
 
-        // MPI_Bcast(r_pre, nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
+        // perform the summation in eq 3
         for (i = p_start; i < p_end; i++) {
             struct node i_node = nodehead[i];
-            double summation = 0;
-
+            r[i] = 0;
             for (j = 0; j < i_node.num_in_links; j++) {
-                summation += r_pre[i_node.inlinks[j]]/(nodehead[i_node.inlinks[j]].num_out_links);
+                r[i] += summation[i_node.inlinks[j]];
             }
-            r[i] = ((1 - DAMPING_FACTOR)/nodecount) + DAMPING_FACTOR * summation;
+            r[i] += DAMP_START_PROB;
         }
-        MPI_Allgatherv(r + p_start, p_end - p_start, MPI_DOUBLE, global_r, recvcounts, disp, MPI_DOUBLE, MPI_COMM_WORLD);
-        for (int test = 0; test < nodecount; test++) {
-            r[test] = global_r[test];
+
+        // gather the updated r values from all processes
+        MPI_Allgatherv(r + p_start, p_end - p_start, MPI_DOUBLE, global_r, part_sizes, displacement, MPI_DOUBLE, MPI_COMM_WORLD);
+        
+        // update local_r with global_r, and calculate the summation values for the next iteration
+        for (int i = 0; i < nodecount; i++) { 
+            r[i] = global_r[i];
+            summation[i] = DAMPING_FACTOR * r[i] / nodehead[i].num_out_links;
         }
         
-    }while(rel_error(r, r_pre, nodecount) >= EPSILON);
-    MPI_Finalize();
-    GET_TIME(end);
+    }while(rel_error(r, r_pre, nodecount) >= EPSILON); // continue until the error is less than EPSILON
+    MPI_Finalize(); // end the shared execution environment
+    GET_TIME(end); // only measure the time required to run the PageRank algorithm and the necessary distributed computing overhead
 
     Lab4_saveoutput(r, nodecount, end - start);
 
     // post processing
     node_destroy(nodehead, nodecount);
-    free(r); free(r_pre); free(global_r);
+    free(r); free(r_pre); free(global_r); free(summation);
     return 0;
 }
